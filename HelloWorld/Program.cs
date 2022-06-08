0ec034cdab377ed9d6ef8b725ef889f032a26926
francis mcdermott <francismcd97@gmail.com>
2022-06-08 12:23:03 +0100

    Test

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,9 +4,13 @@ namespace HelloWorld
 {
     internal class Program
     {
+        // Entry point of our program
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Francis!");
+            Console.WriteLine("Hello Francis!");
+            Console.Read();
         }
     }
 }

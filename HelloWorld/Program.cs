a12004471c84f6d3e98ed391952923f2a5b24162
francis mcdermott <francismcd97@gmail.com>
2022-06-08 13:22:25 +0100

    Played around with console formating.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HelloWorld
 {
@@ -10,14 +14,33 @@ namespace HelloWorld
             //Declare variables
             string myname = "Francis";
             string message = "My name is ";
-
+            
             //convert message to upper case
             string capsMessage = message.ToUpper();
             //convert message to lower case
             string lowwerCaseMessage = message.ToLower();
 
+            //change console foreground color to blue
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            //change background color of console
+            Console.BackgroundColor = ConsoleColor.Blue;
+            //console.Clear();
+            
+
+            //write to console the message in lower case followed by my name
             Console.WriteLine(lowwerCaseMessage + myname);
-            Console.Read();
+
+            //take value from console line
+            string consoleInput = Console.ReadLine().ToString();
+
+            //convert console input to upper
+            string capsConsoleInput = consoleInput.ToUpper();
+
+            //print result in console
+            Console.WriteLine("Output {0}", capsConsoleInput);
+            Console.ReadKey();
+
+           
         }
     }
 }
